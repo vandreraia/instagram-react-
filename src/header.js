851062dd00535d66b10bc63b37979ca7aa029717
Vandré <vandreraia@gmail.com>
2022-05-04 22:03:03 -0300

    first commit

---
+++ src/header.js
@@ -0,0 +1,21 @@
+
+function Header() {
+    return (
+        <header>
+    <div class="logo">
+        <ion-icon name="logo-instagram"></ion-icon>
+        <div class="header-left"><img src="Imgs/logo.png"></div>
+    </div>
+    <div class="header-center">
+        <img src="Imgs/logo.png">
+        <input type="text" placeholder="Pesquisar">
+    </div>
+    <div class="header-right">
+        <ion-icon name="paper-plane-outline"></ion-icon>
+        <ion-icon class="icon-mobile" name="compass-outline"></ion-icon>
+        <ion-icon class="icon-mobile" name="heart-outline"></ion-icon>
+        <ion-icon class="icon-mobile" name="person-outline"></ion-icon>
+    </div>
+</header>
+        )
+    }
