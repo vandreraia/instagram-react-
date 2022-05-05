408837400d239537d0231dd4355f678c5b83f906
Vandré <vandreraia@gmail.com>
2022-05-04 23:10:05 -0300

    adcionado para react

---
+++ src/Main.js
@@ -0,0 +1,82 @@
+import Stories from "./Stories"
+
+export default function Main() {
+    return (
+        <main>
+            <Stories />
+            <div>
+                <div class="sidebar">
+                    <div class="sidebar-topo">
+                        <img class="img-sidebar-topo" src="Imgs/catanacomics 1.png" />
+                        <div class="sidebar-span">
+                            <span class="weight-500">catanacomics</span>
+                            <span class="weight-300 lightgray">Catana</span>
+                        </div>
+                    </div>
+                    <div class="sugestoes">
+                        <div class="sugestoes-box">
+                            <p class="weight-500 ppsmol lightgray">Sugestões para você</p>
+                            <p class="weight-500 ppsmol">Ver tudo</p>
+                        </div>
+                        <div class="sugestoes-box">
+                            <div class="sugestoes-box-inside">
+                                <img src="Imgs/badvibesmemes 1.png" />
+                                <div class="sugestoes-box-inside-name">
+                                    <p class="weight-500">bad.vibes.memes</p>
+                                    <p class="ppsmol lightgray">Segue você</p>
+                                </div>
+                            </div>
+                            <p class="weight-500 ppsmol blue">Seguir</p>
+                        </div>
+                        <div class="sugestoes-box">
+                            <div class="sugestoes-box-inside">
+                                <img src="Imgs/chibirdart 1.png" />
+                                <div class="sugestoes-box-inside-name">
+                                    <p class="weight-500">chibirdart</p>
+                                    <p class="ppsmol lightgray">Segue você</p>
+                                </div>
+                            </div>
+                            <p class="weight-500 ppsmol blue">Seguir</p>
+                        </div>
+                        <div class="sugestoes-box">
+                            <div class="sugestoes-box-inside">
+                                <img src="Imgs/razoesparaacreditar 1.png" />
+                                <div class="sugestoes-box-inside-name">
+                                    <p class="weight-500">razoesparaacreditar</p>
+                                    <p class="ppsmol lightgray">Segue você</p>
+                                </div>
+                            </div>
+                            <p class="weight-500 ppsmol blue">Seguir</p>
+                        </div>
+                        <div class="sugestoes-box">
+                            <div class="sugestoes-box-inside">
+                                <img src="Imgs/adorableanimals 1.png" />
+                                <div class="sugestoes-box-inside-name">
+                                    <p class="weight-500">adorableanimals</p>
+                                    <p class="ppsmol lightgray">Segue você</p>
+                                </div>
+                            </div>
+                            <p class="weight-500 ppsmol blue">Seguir</p>
+                        </div>
+                        <div class="sugestoes-box">
+                            <div class="sugestoes-box-inside">
+                                <img src="Imgs/smallcutecats 1.png" />
+                                <div class="sugestoes-box-inside-name">
+                                    <p class="weight-500">smallcutecats</p>
+                                    <p class="ppsmol lightgray">Segue você</p>
+                                </div>
+                            </div>
+                            <p class="weight-500 ppsmol blue">Seguir</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="fontsize-11 lightergray">Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade •
+                            Termos • Localizações • Contas mais relevantes • Hashtags •
+                            Idioma</p>
+                        <p class="fontsize-11 lightergray">© 2021 INSTAGRAM DO FACEBOOK</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import Body from './Body'
+
+ReactDOM.render(<Body />, document.querySelector(".root"));
+
+
+{/*  */}
