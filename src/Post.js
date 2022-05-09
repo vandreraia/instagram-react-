56909c3496f7921243a35bfa69311b7ef32bb841
Vandré <vandreraia@gmail.com>
2022-05-08 21:05:41 -0300

    finished bonus

--- src/Post.js
+++ src/Post.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 export default function Post(props) {
     const [liked, setLiked] = React.useState("");
+    const [outline, setOutline] = React.useState("heart-outline");
     function isLiked() {
-        !liked ? setLiked("red") : setLiked("");
+        liked == "" ? setLiked("red") : setLiked("");
+        outline == "heart-outline" ? setOutline("heart") : setOutline("heart-outline");
     }
     function isLikedImg() {
         setLiked("red");
+        setOutline("heart");
     }
     return (
         <div class="posts border">
@@ -17,16 +20,16 @@ export default function Post(props) {
                 </div>
                 <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
             </div>
-            <img onClick={() => isLiked()} src={props.img} />
+            <img onClick={() => isLikedImg()} src={props.img} />
             <div>
-                <ion-icon onClick={() => isLiked()} class={liked} name="heart"></ion-icon>
+                <ion-icon onClick={() => isLiked()} class={liked} name={outline}></ion-icon>
                 <ion-icon name="chatbubble-outline"></ion-icon>
                 <ion-icon name="paper-plane-outline"></ion-icon>
                 <ion-icon class="post-bottom-right" name="bookmark-outline"></ion-icon>
             </div>
             <div class="post-bottom-like">
                 <img src="Imgs/respondeai.png" />
-                <p>Curtido por <b class={liked}>respondeai</b> e <b>outras 101.523 pessoas</b></p>
+                <p>Curtido por <b>respondeai</b> e <b>outras 101.523 pessoas</b></p>
             </div>
             <div class="coments">
                 <div class="coment">
